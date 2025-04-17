5644e444a1d7b3c662baf8f80c1547231eb144e3
Antonio Olivari <jolivari.ext@acciona.com>
2025-04-17 13:02:13 +0200

    feat: add testing capabilities and improve SmartPhoneCard component
    - Added Jest and React Testing Library for unit testing.
    - Implemented tests for SmartPhoneCard component to verify rendering and image loading behavior.
    - Enhanced SmartPhoneCard with loading state management for images.
    - Introduced SmartPhoneGrid component with responsive grid layout for displaying multiple SmartPhoneCard components.
    - Updated package.json to include necessary dependencies for testing.
    - Added CSS styles for SmartPhoneGrid layout.
    - Modified SmartPhoneCard styles to include a border for better visual separation.
    - Updated TypeScript configuration to target ES6 and use CommonJS modules.

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
