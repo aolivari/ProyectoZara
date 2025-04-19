2f923f01be1c5663e0e1c541f4fbad87598259e1
Antonio Olivari <jolivari.ext@acciona.com>
2025-04-19 02:56:58 +0200

    feat: actualizar SearchBar para incluir funcionalidad de búsqueda y limpieza, refactorizar estilos y estructura

--- src/_components/SearchBar.tsx
+++ src/_components/SearchBar.tsx
@@ -4,19 +4,36 @@ import styles from '../CSS.modules/SearchBar.module.css';
 interface SearchBarProps {
   onSearch: (query: string) => void;
   resultsCount: string;
+  value: string;
+  clearSearch: () => void;
 }
 
-export const SearchBar = ({ onSearch, resultsCount }: SearchBarProps) => {
+export const SearchBar = ({
+  onSearch,
+  resultsCount,
+  value,
+  clearSearch,
+}: SearchBarProps) => {
   //to do crear los llamado a la base de datos para obtener el total de resultados
 
   return (
     <div role="search" className={styles.searchBar}>
-      <input
-        type="text"
-        placeholder="Search for a smartphone..."
-        className="searchBar"
-        onChange={(e) => onSearch(e.target.value)}
-      />
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          width: '100%',
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Search for a smartphone..."
+          className="searchBar"
+          value={value}
+          onChange={(e) => onSearch(e.target.value)}
+        />
+        {value && <span onClick={() => clearSearch()}>X</span>}
+      </div>
 
       <p>{resultsCount} results</p>
     </div>
--- src/views/SmartPhonesList.tsx
+++ src/views/SmartPhonesList.tsx
@@ -4,12 +4,15 @@ import { SmartPhoneGrid } from '../_components/SmartPhoneGrid';
 import { useGetSmartPhoneList } from '../_hooks/useGetSmartphoneList';
 
 export const SmartPhonesList = () => {
-  const { smartPhoneData, setSearchData } = useGetSmartPhoneList('products');
+  const { smartPhoneData, setSearchData, searchData } =
+    useGetSmartPhoneList('products');
   return (
     <>
       <SearchBar
         onSearch={setSearchData}
         resultsCount={smartPhoneData?.length.toString() ?? '0'}
+        value={searchData as string}
+        clearSearch={() => setSearchData('')}
       />
       <div style={{ marginBottom: 473 }}>
         <SmartPhoneGrid smartPhones={smartPhoneData ?? []} />
